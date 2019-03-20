7d574d5581605437e18ec476a185f383ff40a94c
hilbert25 <hht@pku.edu.cn>
2019-03-20 20:00:43 +0800

    add address

--- blockData/main.go
+++ blockData/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/btcsuite/btcd/rpcclient"
 	"github.com/btcsuite/btcutil"
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	//"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"io/ioutil"
 	"log"
 	"path/filepath"
@@ -25,22 +25,24 @@ func Clear(db *sql.DB) {
 	stmt.Exec()
 }
 
-func Insert(db *sql.DB,blockId int64,blockHash string,miner string, nonce uint32, bits string,timeStamp int64) {
-	stmt, err := db.Prepare("INSERT INTO block(blockId, blockHash,miner,nonce,bits,createTime) VALUES(?, ?,?,?,?,?);")
+func insert(db *sql.DB,blockId int64,blockHash string,miner string, nonce uint32, bits string,timeStamp int64) {
+	fmt.Println(miner)
+	stmt, err := db.Prepare("INSERT INTO block(blockId, blockHash,miner,nonce,bits,createTime) VALUES(?,?,?,?,?,?);")
 	res, err := stmt.Exec(blockId,blockHash,miner,nonce,bits,timeStamp)
+	fmt.Println(res,err)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err,res)
 	}
-	fmt.Println(res)
+	//fmt.Println(res)
 }
 func main() {
 
 	db,_ := sql.Open("mysql", "root:root@/blockchain")
-	Clear(db)
+	//Clear(db)
 	btcdHomeDir := btcutil.AppDataDir("btcd", false)
 	certs, err := ioutil.ReadFile(filepath.Join(btcdHomeDir, "rpc.cert"))
 	if err != nil {
-		//log.Fatal(err)
+		log.Fatal(err)
 	}
 
 	// Create a new RPC client using websockets.  Since this example is
@@ -57,23 +59,21 @@ func main() {
 	if err != nil {
 		//log.Fatal(err)
 	}
-	defer client.Shutdown()
+
 
 	// Query the RPC server for the current block count and display it.
 	//blockCount ,_ := client.GetBlockCount()
-	for blockId := int64(0); blockId< 0;blockId ++ {
+	for blockId := int64(1); blockId<= 1;blockId ++ {
 		blockHash,_ := client.GetBlockHash(blockId)
 		block,_ := client.GetBlockVerbose(blockHash)
 		nonce := block.Nonce
 		bits := block.Bits
 		timeStamp := block.Time
-		txHash,_ := chainhash.NewHashFromStr(block.Tx[0])
-		txOut,_ := client.GetTxOut(txHash,0,true)
-		fmt.Println(txOut)
-		miner := txOut.ScriptPubKey.Addresses[0]
-		Insert(db,blockId,block.Hash,miner,nonce,bits,timeStamp)
-		fmt.Println(blockId,block.Hash,miner,nonce,bits,timeStamp)
+		//txHash,_ := chainhash.NewHashFromStr(block.Tx[0])
+		//tx,_:= client.GetRawTransactionVerbose(txHash)
+		//miner := tx.Vout[0].ScriptPubKey.Addresses[0]
+		insert(db,blockId,block.Hash,block.Hash,nonce,bits,timeStamp)
 	}
 
-
+	defer client.Shutdown()
 }
