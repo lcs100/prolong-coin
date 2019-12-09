a290463466dc6c13bd97e4341e1c5381d1e81766
liuchenshuo <liuchenshuo@4paradigm.com>
2019-12-09 16:46:06 +0800

    fourth modify

--- pre-install.sh
+++ pre-install.sh
@@ -12,12 +12,16 @@ wget https://dl.google.com/go/go1.11.5.linux-amd64.tar.gz
 tar xzf go1.11.5.linux-amd64.tar.gz -C /usr/local
 
 # 修改bashrc写入相关go环境变量
-echo "go Path setting" >> ~/.bashrc
+echo " #go Path setting" >> ~/.bashrc
 echo "export GOPATH=$HOME/go" >> ~/.bashrc
 echo "export GOROOT=/usr/local/go" >> ~/.bashrc
+GOROOT=$HOME/go
 echo "export GOBIN=$GOROOT/bin" >> ~/.bashrc
 echo "export GOTOOLS=$GOROOT/pkg/tool" >> ~/.bashrc
+GOTOOLS=$GOROOT/pkg/tool
 echo "export PATH=$PATH:$GOBIN:$GOTOOLS" >> ~/.bashrc
+
+source ~/.bashrc
 
 # 执行另外一个脚本
 cd prolong-coin/github.com/btcsuite/btcd
